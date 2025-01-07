9b5d21dfe80599cdba7ed50e70adaeda917fc4ba
Ibrahima Sarr <114916109+eutousarr@users.noreply.github.com>
2025-01-07 05:12:21 +0000

    dev

--- prisma/migrations/20250107051005_initial/migration.sql
+++ prisma/migrations/20250107051005_initial/migration.sql
@@ -1,15 +1,13 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    "email" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Invoice" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "issuerName" TEXT NOT NULL DEFAULT '',
     "issuerAddress" TEXT NOT NULL DEFAULT '',
@@ -18,29 +16,21 @@ CREATE TABLE "Invoice" (
     "invoiceDate" TEXT NOT NULL DEFAULT '',
     "dueDate" TEXT NOT NULL DEFAULT '',
     "vatActive" BOOLEAN NOT NULL DEFAULT false,
-    "vatRate" DOUBLE PRECISION NOT NULL DEFAULT 20,
+    "vatRate" REAL NOT NULL DEFAULT 20,
     "status" INTEGER NOT NULL DEFAULT 1,
     "userId" TEXT NOT NULL,
-
-    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Invoice_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "InvoiceLine" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "description" TEXT NOT NULL DEFAULT '',
     "quantity" INTEGER NOT NULL DEFAULT 0,
-    "unitPrice" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "unitPrice" REAL NOT NULL DEFAULT 0,
     "invoiceId" TEXT,
-
-    CONSTRAINT "InvoiceLine_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "InvoiceLine_invoiceId_fkey" FOREIGN KEY ("invoiceId") REFERENCES "Invoice" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- AddForeignKey
-ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "InvoiceLine" ADD CONSTRAINT "InvoiceLine_invoiceId_fkey" FOREIGN KEY ("invoiceId") REFERENCES "Invoice"("id") ON DELETE CASCADE ON UPDATE CASCADE;
